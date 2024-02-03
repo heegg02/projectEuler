801442826f139e08c1668ae77703911e03c30440
HEEgg02 <h111kyung9@gmail.com>
2024-02-03 20:15:56 +0900

    Solved problem 003 at level.0 in programmers

---
+++ app/src/main/java/programers/Problem0_003.java
@@ -0,0 +1,21 @@
+package programers;
+
+public class Problem0_003 {
+    
+    public static void main(String[] args) {
+        var string = "AbCdEfG_= Ab   s";
+        //////////////////////////////////////////
+        String str = "";
+        
+        for(char c : string.toCharArray()) {
+            if(Character.isUpperCase(c)) {
+                str += Character.toLowerCase(c);
+            } else if(Character.isLowerCase(c)){
+                str += Character.toUpperCase(c);
+            } else {
+                str += c;
+            }
+        }
+        System.out.println(str);
+    }
+}
