86994a94c361f85d78aa91fc625046f0208627de
HEEgg02 <h111kyung9@gmail.com>
2024-02-03 19:01:39 +0900

    Solve problem001

---
+++ app/src/main/java/projecteuler/Problem001.java
@@ -0,0 +1,27 @@
+package projecteuler;
+import java.math.BigDecimal;
+
+public class Problem001 {
+
+    public static void main(String[] args) {
+        var beforeTime = System.nanoTime();
+
+        ////////////////////////////////////////
+        int num = 0;
+        for(int i=0; i<=999; i++) {
+            if(i%3==0||i%5==0) {
+                num+=i;
+            }
+        }
+        System.out.println(num);
+        ////////////////////////////////////////
+
+        var interval = System.nanoTime() - beforeTime;
+        var d = new BigDecimal(interval);
+        var c = new BigDecimal(10000000000L);
+        var toSec = d.divide(c);
+
+        System.out.println("code run timestamp: " + d + "(ns)");
+        System.out.println("code run timestamp: " + toSec + "(s)");
+    }
+}
