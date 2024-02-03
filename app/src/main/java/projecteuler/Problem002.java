433e5f4d575b0469413bd09997192f8409b9c505
HEEgg02 <h111kyung9@gmail.com>
2024-02-03 21:15:55 +0900

    Updated Corby's version

--- app/src/main/java/projecteuler/Problem002.java
+++ app/src/main/java/projecteuler/Problem002.java
@@ -20,5 +20,27 @@ public class Problem002 {
             }
         }
         System.out.println(result);
+        fn();
+    }
+
+    // Corby가 푼것.
+    public static void fn() {
+        int i;
+        int num1 = 1;
+        int num2 = 2;
+        var sum = 0;
+
+        for(i = num2; sum <= 4000000; i += (sum % 2 == 0) ? sum : 0) {
+            
+            // add numbers
+            sum = num1 + num2;
+
+            // swap
+            var temp = num2;
+            num2 += num1; 
+            num1 = temp;
+        }
+
+        System.out.println(i);
     }
 }
