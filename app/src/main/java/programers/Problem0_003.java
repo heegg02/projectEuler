92937fed1dc23edd7b45ed1ca8f0affe5abdd064
Hwanhee Lee <157552253+heegg02@users.noreply.github.com>
2024-02-04 20:04:34 +0900

    Update Problem0_003.java

--- app/src/main/java/programers/Problem0_003.java
+++ app/src/main/java/programers/Problem0_003.java
@@ -3,11 +3,11 @@ package programers;
 public class Problem0_003 {
     public static void main(String[] args) {
       
-    String str = "string";
-    int n = 5;
-
-    for(int i=0; i<n; i++) {
-        System.out.print(str);
-    }  
+        String str = "string";
+        int n = 5;
+    
+        for(int i=0; i<n; i++) {
+            System.out.print(str);
+        }  
     }
 }
