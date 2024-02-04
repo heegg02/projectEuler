2dc0d348ab44046ee4692e461ed440ad2099ed9e
HEEgg02 <h111kyung9@gmail.com>
2024-02-04 20:03:18 +0900

    변경 프로그래머스 level.0 003

--- app/src/main/java/programers/Problem0_003.java
+++ app/src/main/java/programers/Problem0_003.java
@@ -2,11 +2,12 @@ package programers;
 
 public class Problem0_003 {
     public static void main(String[] args) {
-        String str = "string";
-        int n = 5;
+      
+    String str = "string";
+    int n = 5;
 
-        for(int i=0; i<n; i++) {
-            System.out.print(str);
-        }  
+    for(int i=0; i<n; i++) {
+        System.out.print(str);
+    }  
     }
 }
